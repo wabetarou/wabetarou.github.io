7951f7309aa743ee72b09594805952ded3499079
nozzle <nozzlex3@gmail.com>
2021-02-09 17:04:35 +0900

    仕様追加：タグページの追加、ポストの表示のコンポーネント化

---
+++ src/components/post-column.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const PostColumnTemplate = ({ node }) => {
+  //   const { node } = data.allMarkdownRemark.edges
+  return (
+    <li key={node.fields.slug}>
+      <hr />
+      <article
+        className="post-list-item"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h2>
+            <Link to={node.fields.slug} itemProp="url">
+              <span itemProp="headline">{node.frontmatter.title}</span>
+            </Link>
+          </h2>
+          <small>{node.frontmatter.created}</small>
+          <div className="tag-list">
+            {node.frontmatter.tag?.map(tag => {
+              return (
+                <div className="tag">
+                  <small>
+                    <Link to={`/tags/${tag}/`}>{tag}</Link>
+                  </small>
+                </div>
+              )
+            })}
+          </div>
+        </header>
+        <section>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: node.frontmatter.description || node.excerpt,
+            }}
+            itemProp="description"
+          />
+        </section>
+      </article>
+    </li>
+  )
+}
+export default PostColumnTemplate
