d8a11db61ea5469f877170c60f927e756ef9b375
nozzle <nozzlex3@gmail.com>
2021-02-09 15:39:01 +0900

    機能追加：各タグページ、全タグページ、各タグへのリンク

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+// const _ = require("lodash")
+
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -50,28 +52,9 @@ const BlogIndex = ({ data, location }) => {
                     {post.frontmatter.tag?.map(tag => {
                       return (
                         <div className="tag">
-                          {/* <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="64"
-                            height="64"
-                            viewBox="0 0 64 64"
-                          >
-                            <path
-                              fill="#fff"
-                              stroke="#231815"
-                              d="M-8.75-5h48.813"
-                            ></path>
-                            <path
-                              fill="#2b932b"
-                              fillRule="evenodd"
-                              d="M-25.333 18.781c-1.657 0-13-9.969-13-11.375s11.343-11.375 13-11.375"
-                            ></path>
-                            <path
-                              fill="#555"
-                              d="M53.59 33.012L30.596 10.02c-1.11-1.11-16.875 1.452-18 2.577s-3.687 16.889-2.577 18L33.013 53.59c1.11 1.11 3.224.797 4.724-.703l15.148-15.148c1.5-1.501 1.815-3.617.705-4.727zm-31.403-5.7a4.875 4.875 0 11.001-9.751 4.875 4.875 0 01-.001 9.751z"
-                            ></path>
-                          </svg> */}
-                          <small>{tag}</small>
+                          <small>
+                            <Link to={`/tags/${tag}/`}>{tag}</Link>
+                          </small>
                         </div>
                       )
                     })}
